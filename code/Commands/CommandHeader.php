7ab9ad45f4e5fef941bdd8286625d9020ec16c26
Madsen, Kent v <kent.vejrup.madsen@designermadsen.dk>
2022-10-13 15:03:46 +0200

    added file for redirect

---
+++ code/Commands/CommandHeader.php
@@ -0,0 +1,9 @@
+<?php
+    namespace IoJaegers\Utilities\Commands;
+
+
+    abstract class CommandHeader
+    {
+        public abstract function execute(): never;
+    }
+?>
---
+++ code/Commands/RedirectToHeader.php
@@ -0,0 +1,55 @@
+<?php
+    namespace IoJaegers\Utilities\Commands;
+
+
+    /**
+     *
+     */
+    class RedirectToHeader
+        extends CommandHeader
+    {
+        /**
+         * @param $toLink
+         */
+        public function __construct( $toLink )
+        {
+            $this->setRedirectTo( $toLink );
+        }
+
+        // Variables
+        private string|null $redirectTo = null;
+
+
+        /**
+         * @return never
+         * @throws \Exception
+         */
+        public function execute(): never
+        {
+            if( $this->getRedirectTo() == null )
+            {
+                throw new \Exception('RedirectToHeader can not be null.');
+            }
+
+            header('Location: ' . $this->getRedirectTo() );
+            exit();
+        }
+
+
+        /**
+         * @return string|null
+         */
+        public final function getRedirectTo(): ?string
+        {
+            return $this->redirectTo;
+        }
+
+        /**
+         * @param string|null $redirectTo
+         */
+        public final function setRedirectTo( ?string $redirectTo ): void
+        {
+            $this->redirectTo = $redirectTo;
+        }
+    }
+?>
